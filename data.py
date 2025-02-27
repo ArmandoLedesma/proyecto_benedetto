9f79debf9ac6770bfd7fb3995ac8e41ceb48e69a
danieltrejos <tmdaneil@gmail.com>
2025-02-26 21:29:17 -0500

    Templates

--- data.py
+++ data.py
@@ -30,3 +30,249 @@ items = [
         "button_text": "Gestionar Sucursales"
     }
 ]
+
+items_categorias = [
+    {
+        "image": "static/img/categoria/categorias.jpg",
+        "title": "Pizzas",
+        "description": "Esta es la descripción de la primera tarjeta.",
+        "button_text": "Gestionar Categorias"
+    },
+    {
+        "image": "static/img/empleados/empleados.jpg",
+        "title": "Hamburguesas",
+        "description": "Esta es la descripción de la segunda tarjeta.",
+        "button_text": "Gestionar Empleados"
+    },
+    {
+        "image": "static/img/cliente/cliente.svg",
+        "title": "Lasañas",
+        "description": "Esta es la descripción de la primera tarjeta.",
+        "button_text": "Gestionar Clientes"
+    },
+    {
+        "image": "static/img/l_productos/producto.jpg",
+        "title": "Picadas",
+        "description": "Esta es la descripción de la primera tarjeta.",
+        "button_text": "Gestionar Linea de productos"
+    },
+    {
+        "image": "static/img/sucursal/sucursales.svg",
+        "title": "Sucursales",
+        "description": "Esta es la descripción de la segunda tarjeta.",
+        "button_text": "Gestionar Sucursales"
+    },
+    {
+        "image": "static/img/sucursal/sucursales.svg",
+        "title": "Sucursales",
+        "description": "Esta es la descripción de la segunda tarjeta.",
+        "button_text": "Gestionar Sucursales"
+    },
+    {
+        "image": "static/img/sucursal/sucursales.svg",
+        "title": "Sucursales",
+        "description": "Esta es la descripción de la segunda tarjeta.",
+        "button_text": "Gestionar Sucursales"
+    }
+]
+
+
+
+items_empleados = [
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "cargo": "mesero",
+        "salario": "2000000",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com"
+    },
+    {
+        "id": "10002",
+        "nombre": "Armando Benedeti",
+        "cargo": "administraidor",
+        "salario": "500000000",
+        "telefono": "03251515",
+        "email": "micorreo2@gmail.com"
+    }
+]
+
+
+items_clientes = [
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+    {
+        "id": "10001",
+        "nombre": "Daniel 3",
+        "telefono": "5613513613",
+        "email": "micorreo@gmail.com",
+        "estado": "Activo",
+        "empleado": "Daniel",
+        "categoria": "Hamburguesa"
+    },
+]
+
+items_sucursales = [
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+    {
+        "nombre_sucursal": "Vivaa",
+        "numero_sucursal": "1",
+        "capacidad": "100",
+        "direccion": "Avenida siempre viva 123",
+        "telefono": "0000000001",
+    },
+]
