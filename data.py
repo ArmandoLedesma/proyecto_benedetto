b61301e6fa820419d1bcfb5edac274e7b7d2a864
ArmandoLedesma <armandojledesma26@gmail.com>
2025-02-27 21:36:46 -0500

    Actualización de categorias y modelado de datos

--- data.py
+++ data.py
@@ -33,34 +33,34 @@ items = [
 
 items_categorias = [
     {
-        "image": "static/img/categoria/categorias.jpg",
         "title": "Pizzas",
-        "description": "Esta es la descripción de la primera tarjeta.",
-        "button_text": "Gestionar Categorias"
+        "image": "static/img/categorias_subcategorias/pizza.jpg",
+        "description": "Pizzas irresistibles: masa artesanal, ingredientes frescos y el equilibrio perfecto de sabores. 🍕🔥 Un solo bocado y te conquista.",
+        "button_text": "Ver más"
     },
     {
-        "image": "static/img/empleados/empleados.jpg",
         "title": "Hamburguesas",
-        "description": "Esta es la descripción de la segunda tarjeta.",
-        "button_text": "Gestionar Empleados"
+        "image": "static/img/categorias_subcategorias/hamburguesas.jpg",
+        "description": "Hamburguesas irresistibles: carne jugosa, pan esponjoso y el equilibrio perfecto de ingredientes. 🍔🔥 Un solo bocado y te enamoras.",
+        "button_text": "Ver más"
     },
     {
-        "image": "static/img/cliente/cliente.svg",
-        "title": "Lasañas",
-        "description": "Esta es la descripción de la primera tarjeta.",
-        "button_text": "Gestionar Clientes"
+        "title": "Lazañas",
+        "image": "static/img/categorias_subcategorias/lazana.jpg",
+        "description": "Lasañas irresistibles: pasta suave, salsa cremosa y el gratinado perfecto. 🍽️🔥 Un solo bocado y te atrapa.",
+        "button_text": "Ver más"
     },
     {
-        "image": "static/img/l_productos/producto.jpg",
-        "title": "Picadas",
-        "description": "Esta es la descripción de la primera tarjeta.",
-        "button_text": "Gestionar Linea de productos"
+        "title": "Salchipapas",
+        "image": "static/img/categorias_subcategorias/salchipapa.jpg",
+        "description": "Salchipapas irresistibles: papas crujientes, salchichas doradas y el toque perfecto de salsas. 🍟🔥 Un solo bocado y te envuelve.",
+        "button_text": "Ver más"
     },
     {
-        "image": "static/img/sucursal/sucursales.svg",
-        "title": "Sucursales",
-        "description": "Esta es la descripción de la segunda tarjeta.",
-        "button_text": "Gestionar Sucursales"
+        "title": "Bebidas",
+        "image": "static/img/categorias_subcategorias/bebidas.avif",
+        "description": "Bebidas irresistibles: refrescantes, intensas y el complemento perfecto para cada antojo. 🥤🔥 Un solo sorbo y lo disfrutas.",
+        "button_text": "Ver más"
     },
     {
         "image": "static/img/sucursal/sucursales.svg",
@@ -74,6 +74,206 @@ items_categorias = [
         "description": "Esta es la descripción de la segunda tarjeta.",
         "button_text": "Gestionar Sucursales"
     }
+]
+
+pizza_tradicionales = [
+
+    {
+        "image": "",
+        "nombre": "Hawaiana",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 12500
+
+    },
+    {
+        "image": "",
+        "nombre": "Jamón queso",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 12500
+
+    },
+    {
+        "image": "",
+        "nombre": "Napolitana",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 12500
+
+    },
+    {
+        "image": "",
+        "nombre": "Vegetariana",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 13500
+
+    },
+    {
+        "image": "",
+        "nombre": "Jamón pollo",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 14000
+
+    },
+    {
+        "image": "",
+        "nombre": "Pollo champiñón",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 14000
+
+    },
+    {
+        "image": "",
+        "nombre": "Pepperoni",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 14500
+
+    },
+    {
+        "image": "",
+        "nombre": "Hawaiana pollo BBQ",
+        "descripcion": "",
+        "categoria": "Tradicional",
+        "precio": 15000
+
+    },
+    
+]
+
+pizza_autor = [
+    {
+        "image": "",
+        "nombre": "Remontada",
+        "descripcion": "Pollo, maiz, tartara y papa de cabello de ángel",
+        "categoria": "De autor",
+        "precio": 14000
+    },
+    {
+        "image": "",
+        "nombre": "Especial",
+        "descripcion": "Pollo, salami, cábano, champiñón, cebolla caramelizada y pimentón",
+        "categoria": "De autor",
+        "precio": 15500
+    },
+    {
+        "image": "",
+        "nombre": "Hawaiana Plus",
+        "descripcion": "Tocineta, jamón, piña y parmesano",
+        "categoria": "De autor",
+        "precio": 14500
+    },
+    {
+        "image": "",
+        "nombre": "Pollo funghi",
+        "descripcion": " Pollo, champiñones salteados, mayotrufa y parmesano",
+        "categoria": "De autor",
+        "precio": 15500
+    },
+    {
+        "image": "",
+        "nombre": "Mexicana",
+        "descripcion": "",
+        "categoria": "De autor",
+        "precio": 14500
+    },
+    {
+        "image": "",
+        "nombre": "Benedetto",
+        "descripcion": "Pepperoni, prosciutto y cebolla caramelizada",
+        "categoria": "De autor",
+        "precio": 16000
+    },
+    {
+        "image": "",
+        "nombre": "Suprema",
+        "descripcion": "Pepperoni, aceitunas, champiñón, cebolla encurtida y pimentón",
+        "categoria": "De autor",
+        "precio": 14500
+    },
+    {
+        "image": "",
+        "nombre": "Toscana",
+        "descripcion": "Tocineta, jamón, maíz, champiñón y parmesano",
+        "categoria": "De autor",
+        "precio": 15000
+    },
+    {
+        "image": "",
+        "nombre": "Pollo miel mostaza",
+        "descripcion": "",
+        "categoria": "De autor",
+        "precio": 15000
+    },
+    {
+        "image": "",
+        "nombre": "Romana",
+        "descripcion": "Pollo, tocineta, maíz y orégano",
+        "categoria": "De autor",
+        "precio": 15000
+    },
+    {
+        "image": "",
+        "nombre": "Americana",
+        "descripcion": "Panceta, jalapeños, cebolla roja, cilantro y salsa BBQ",
+        "categoria": "De autor",
+        "precio": 15500
+    },
+    {
+        "image": "",
+        "nombre": "Caramellata",
+        "descripcion": "Tocineta, manzana caramelizada, tomate, dulce, parmesano y albahaca",
+        "categoria": "De autor",
+        "precio": 15500
+    },
+    {
+        "image": "",
+        "nombre": "Carnes",
+        "descripcion": "Pepperoni, tocineta, jamón, cábano y salchicha",
+        "categoria": "De autor",
+        "precio": 15500
+    },
+    {
+        "image": "",
+        "nombre": "Diletto",
+        "descripcion": "Pepperoni, pollo, tocineta y jamón",
+        "categoria": "De autor",
+        "precio": 16500
+    },
+    {
+        "image": "",
+        "nombre": "Mistura",
+        "descripcion": "Reducción de panela, pepperoni, chorizo español, queso crema y albahaca",
+        "categoria": "De autor",
+        "precio": 18500
+    },
+    {
+        "image": "",
+        "nombre": "Stracciatella",
+        "descripcion": "Tomates caramelizados, stracciatella, parmesano y albahaca",
+        "categoria": "De autor",
+        "precio": 18500
+    },
+    {
+        "image": "",
+        "nombre": "Genovesa ",
+        "descripcion": " Prosciutto, salsa pesto, tomates confitados, stracciatella y parmesano",
+        "categoria": "De autor",
+        "precio": 20000
+    },
+    {
+        "image": "",
+        "nombre": "Miel al tartufo",
+        "descripcion": "Pepperoni, prosciutto, stracciatella, miel de trufas y parmesano",
+        "categoria": "De autor",
+        "precio": 19000
+    },
+    
+    
 ]
 
 
