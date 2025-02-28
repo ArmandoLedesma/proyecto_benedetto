1c6bb7071b8d53298efab33c6b7b8ae8eef54ff1
danieltrejos <tmdaneil@gmail.com>
2025-02-28 11:35:15 -0500

    Actualizacion de las rutas de las vistas, modificacion de las carpetas de los templates

--- data.py
+++ data.py
@@ -1,30 +1,32 @@
 items = [
     {
-        "image": "static/img/img_dashboard/categoria/categorias.jpg",
+
+        "image": "img/img_dashboard/categoria/categorias.jpg",
+
         "title": "Categorias",
         "description": "Esta es la descripción de la primera tarjeta.",
         "button_text": "Gestionar Categorias"
     },
     {
-        "image": "static/img/img_dashboard/empleados/empleados.jpg",
+        "image": "img/img_dashboard/empleados/empleados.jpg",
         "title": "Empleados",
         "description": "Esta es la descripción de la segunda tarjeta.",
         "button_text": "Gestionar Empleados"
     },
     {
-        "image": "static/img/img_dashboard/cliente/clientes.jpg",
+        "image": "img/img_dashboard/cliente/clientes.jpg",
         "title": "Clientes",
         "description": "Esta es la descripción de la primera tarjeta.",
         "button_text": "Gestionar Clientes"
     },
     {
-        "image": "static/img/img_dashboard/l_productos/productos.jpg",
+        "image": "img/img_dashboard/l_productos/productos.jpg",
         "title": "Linea de productos",
         "description": "Esta es la descripción de la primera tarjeta.",
         "button_text": "Gestionar Linea de productos"
     },
     {
-        "image": "static/img/img_dashboard/sucursal/sucursales.jpg",
+        "image": "img/img_dashboard/sucursal/sucursales.jpg",
         "title": "Sucursales",
         "description": "Esta es la descripción de la segunda tarjeta.",
         "button_text": "Gestionar Sucursales"
@@ -34,42 +36,42 @@ items = [
 items_categorias = [
     {
         "title": "Pizzas",
-        "image": "static/img/categorias_subcategorias/pizza.jpg",
+        "image": "img/categorias_subcategorias/pizza.jpg",
         "description": "Pizzas irresistibles: masa artesanal, ingredientes frescos y el equilibrio perfecto de sabores. 🍕🔥 Un solo bocado y te conquista.",
         "button_text": "Ver más"
     },
     {
         "title": "Hamburguesas",
-        "image": "static/img/categorias_subcategorias/hamburguesas.jpg",
+        "image": "img/categorias_subcategorias/hamburguesas.jpg",
         "description": "Hamburguesas irresistibles: carne jugosa, pan esponjoso y el equilibrio perfecto de ingredientes. 🍔🔥 Un solo bocado y te enamoras.",
         "button_text": "Ver más"
     },
     {
         "title": "Lazañas",
-        "image": "static/img/categorias_subcategorias/lazana.jpg",
+        "image": "img/categorias_subcategorias/lazana.jpg",
         "description": "Lasañas irresistibles: pasta suave, salsa cremosa y el gratinado perfecto. 🍽️🔥 Un solo bocado y te atrapa.",
         "button_text": "Ver más"
     },
     {
         "title": "Salchipapas",
-        "image": "static/img/categorias_subcategorias/salchipapa.jpg",
+        "image": "img/categorias_subcategorias/salchipapa.jpg",
         "description": "Salchipapas irresistibles: papas crujientes, salchichas doradas y el toque perfecto de salsas. 🍟🔥 Un solo bocado y te envuelve.",
         "button_text": "Ver más"
     },
     {
         "title": "Bebidas",
-        "image": "static/img/categorias_subcategorias/bebidas.avif",
+        "image": "img/categorias_subcategorias/bebidas.avif",
         "description": "Bebidas irresistibles: refrescantes, intensas y el complemento perfecto para cada antojo. 🥤🔥 Un solo sorbo y lo disfrutas.",
         "button_text": "Ver más"
     },
     {
-        "image": "static/img/sucursal/sucursales.svg",
+        "image": "img/sucursal/sucursales.svg",
         "title": "Sucursales",
         "description": "Esta es la descripción de la segunda tarjeta.",
         "button_text": "Gestionar Sucursales"
     },
     {
-        "image": "static/img/sucursal/sucursales.svg",
+        "image": "img/sucursal/sucursales.svg",
         "title": "Sucursales",
         "description": "Esta es la descripción de la segunda tarjeta.",
         "button_text": "Gestionar Sucursales"
--- views/routes_dashboard.py
+++ views/routes_dashboard.py
@@ -1,31 +1,32 @@
-from data import items,items_categorias, items_clientes, items_empleados, items_sucursales
+from data import items, items_categorias, items_clientes, items_empleados, items_sucursales
 
 from flask import Blueprint,render_template 
 
 
-dashboard_bp = Blueprint('dashboard_bp', __name__)
-# dashboard_bp = Blueprint('dashboard_bp', __name__, template_folder="dashboard")
+#dashboard_bp = Blueprint('dashboard_bp', __name__)
+dashboard_bp = Blueprint('dashboard_bp', __name__, template_folder="dashboard")
 
 
-@dashboard_bp.route("/dashboard")
+
+@dashboard_bp.route('/')
 def show_dashboard():
-    return render_template("dashboard.html", items = items)
+    return render_template('dashboard/dashboard.html', items = items, items_categorias= items_categorias)
 
 @dashboard_bp.route("/categorias")
 def show_categorias():
-    return render_template("categorias.html", items= items_categorias)
+    return render_template("dashboard/categorias.html", items= items_categorias)
 
 @dashboard_bp.route("/empleados")
 def show_empleados():
-    return render_template("empleados.html", items= items_empleados)
+    return render_template("dashboard/empleados.html", items= items_empleados)
 
 @dashboard_bp.route("/clientes")
 def show_clientes():
-    return render_template("clientes.html", items= items_clientes)
+    return render_template("dashboard/clientes.html", items= items_clientes)
 
 @dashboard_bp.route("/sucursales")
 def show_sucursales():
-    return render_template("sucursales.html", items= items_sucursales)
+    return render_template("dashboard/sucursales.html", items= items_sucursales)
 
 
 
