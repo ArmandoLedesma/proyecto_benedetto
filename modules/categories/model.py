a0cc4728b51761f9e8ab8a7fcfeceae9eccc210d
danieltrejos <tmdaneil@gmail.com>
2025-03-13 12:30:39 -0500

    Nueva modificación tras el último push

--- modules/categories/model.py
+++ modules/categories/model.py
@@ -2,25 +2,23 @@ from database.db import db
 
 class Categoria(db.Model):
     __tablename__ = 'categorias'
-    # Atributos
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
-    description = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     image = db.Column(db.String(255), nullable=False)
-    button_text = db.Column(db.String(50), nullable=False)
-    url = db.Column(db.String(200), nullable=False)
-    # Estados
-    is_active = db.Column(db.Boolean, nullable=False, default=1)
-    is_deleted = db.Column(db.Boolean, nullable=False, default=0)
-    # Relaciones
-    productos = db.relationship('Producto', backref='categoria', lazy=True)
+    description = db.Column(db.String(255), nullable=False)
+    button_text = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    is_deleted = db.Column(db.Boolean, nullable=False, default=False)
+
+    productos = db.relationship('Producto', back_populates='categoria', lazy=True)
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
-            'description': self.description,
             'image': self.image,
+            'description': self.description,
             'button_text': self.button_text,
             'url': self.url,
             'is_active': self.is_active,
